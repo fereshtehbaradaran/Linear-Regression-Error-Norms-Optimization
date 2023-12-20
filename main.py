15eb34d5554ed123ece5ef965b3d720558076479
fereshtehbaradaran <bara.frsht@gmail.com>
2023-12-20 15:12:28 +0330

    calculate norm functions added

--- main.py
+++ main.py
@@ -7,3 +7,31 @@ np.random.seed(0)
 x = np.linspace(0, 10, 100)
 y_true = 3 * x + 10
 y = y_true + np.random.normal(0, 2, x.shape)  # to add some noise
+
+
+def calculatePredicted_Y(x, alpha, beta):
+    return alpha * x + beta
+
+
+
+def l0_norm(alpha, beta):
+    errors = calculatePredicted_Y(x, alpha, beta) - y
+    return np.sum(errors != 0)
+
+
+
+def l1_norm(alpha, beta):
+    errors = calculatePredicted_Y(x, alpha, beta) - y
+    return np.sum(np.abs(errors))
+
+
+
+def l2_norm(alpha, beta):
+    errors = calculatePredicted_Y(x, alpha, beta) - y
+    return np.sum(np.square(errors))
+
+
+
+def infinity_norm(alpha, beta):
+    errors = calculatePredicted_Y(x, alpha, beta) - y
+    return np.max(np.abs(errors))
